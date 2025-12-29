719263c7bd15e003dd02ebfcff8f945d2612bbf4
IvanGabriel1 <ivangabraun@gmail.com>
2025-12-28 21:14:51 -0300

    Corr Err Sonido Alarmas Programadas - Commit antes de RecordApp Notificaciones programadas, no alarmas

--- src/Context/AlarmaContext.jsx
+++ src/Context/AlarmaContext.jsx
@@ -230,7 +230,7 @@ export const AlarmaProvider = ({ children }) => {
           alarma.minutos === minutosActuales
         ) {
           reproducirSonido(alarma.sonido.nombre);
-          // Podés eliminarla después si querés
+
           return;
         }
 
@@ -241,7 +241,7 @@ export const AlarmaProvider = ({ children }) => {
           alarma.hora === horaActual &&
           alarma.minutos === minutosActuales
         ) {
-          reproducirSonido(alarma.sonido.nombre);
+          reproducirSonido(alarma.sonido?.nombre ?? "Alarma Despetador");
         }
       });
 
